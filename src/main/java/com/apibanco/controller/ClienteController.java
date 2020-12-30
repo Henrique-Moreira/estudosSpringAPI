bd0c892dc7e919e7cfa062c45c68ebfc7e26d254
Henrique Moreira <henriquemoreiraa@gmail.com>
2020-12-30 16:08:07 -0300

    Adiciona funcionalidade de validação dos dados

--- src/main/java/com/apibanco/controller/ClienteController.java
+++ src/main/java/com/apibanco/controller/ClienteController.java
@@ -2,8 +2,12 @@ package com.apibanco.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+import javax.xml.bind.TypeConstraintException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,18 +21,28 @@ import com.apibanco.repository.ClienteRepository;
 @RestController
 @RequestMapping("/clientes")
 public class ClienteController {
-	
+
 	@Autowired
 	private ClienteRepository clienteRepository;
-	
+
 	@GetMapping
 	public List<Cliente> listar() {
 		return clienteRepository.findAll();
 	}
-	
+
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
 	public Cliente adicionar(@RequestBody Cliente cliente) {
 		return clienteRepository.save(cliente);
 	}
+	
+	//@PostMapping
+	//@ResponseStatus(HttpStatus.CREATED)
+	//public Cliente adicionar(@Valid @RequestBody Cliente cliente, BindingResult br) {
+	//	if(br.hasErrors()) {
+	//		throw new TypeConstraintException(br.getAllErrors().get(0).getDefaultMessage());
+	//	} else {
+	//		return clienteRepository.save(cliente);	
+	//	}
+	//}
 }
--- src/main/java/com/apibanco/model/Cliente.java
+++ src/main/java/com/apibanco/model/Cliente.java
@@ -5,6 +5,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 
 import lombok.Data;
 
@@ -17,14 +19,21 @@ public class Cliente {
 	private Long id;
 	
 	@Column(nullable = false)
+	@NotBlank(message = "O nome deve ser preenchido")
 	private String nome;
-
-	@Column(nullable = false)
-	private String email;
 	
 	@Column(nullable = false)
+	@NotBlank(message = "O email deve ser preenchido")
+	@Pattern(regexp = "^([\\w-\\.]+){1,64}@([\\w&&[^_]]+){2,255}.[a-z]{2,}$", message = "Formato de email incorreto.") //validar email. ex: exemplo@dominio.com
+	private String email;
+	
+	@Column(nullable = false) 
+	@NotBlank(message = "O cpf deve ser preenchido")
+	@Pattern(regexp = "(^\\d{3}\\x2E\\d{3}\\x2E\\d{3}\\x2D\\d{2}$)", message = "0 cpf deve conter o formato ddd.ddd.ddd-dd") //validar cpf. ex: 111.222.333-44
 	private String cpf;
 	
 	@Column(nullable = false)
+	@NotBlank(message = "A data de nascimento deve ser preenchida")
+	@Pattern(regexp = "^((19|2[0-9])[0-9]{2})-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])$", message = "A data deve conter o formato aaaa-dd-mm") // intervalo entre 1900-01-01 e 2999-12-31
 	private String dtNasc;
 }
